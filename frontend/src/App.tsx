1f3314e4941af83600732cd1717d50ba5a540e9a
Krak3nDev <lovekuchernazarlove@gmail.com>
2024-02-04 11:53:51 +0200

    added additional functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,48 +3,41 @@ import {BrowserRouter, Route, Routes, useNavigate} from "react-router-dom"
 import {ROUTES} from "./constants/routes.ts"
 import SurveyPage from "./pages/Survey/Survey.tsx"
 import { useSessionStorage } from "react-use"
+import {SURVEY_TEXTS} from "./constants/texts.ts"
 
 
 function App() {
 
-    const [value, setValue, remove] = useSessionStorage("my-key", "default-value")
+    const [qualityRatingPage1, setQualityRatingPage1] = useSessionStorage<number | undefined>("qualityRatingPage1", undefined)
+    const [positiveFeedbackPage1, setPositiveFeedbackPage1] = useSessionStorage<string>("positiveFeedbackPage1", "")
+    const [negativeFeedbackPage1, setNegativeFeedbackPage1] = useSessionStorage<string>("negativeFeedbackPage1", "")
 
+    const [qualityRatingPage2, setQualityRatingPage2] = useSessionStorage<number | undefined>("qualityRatingPage2", undefined)
+    const [positiveFeedbackPage2, setPositiveFeedbackPage2] = useSessionStorage<string>("positiveFeedbackPage2", "")
+    const [negativeFeedbackPage2, setNegativeFeedbackPage2] = useSessionStorage<string>("negativeFeedbackPage2", "")
 
-    const [qualityRatingPage1, setQualityRatingPage1] = useSessionStorage("qualityRatingPage1", null)
-    const [positiveFeedbackPage1, setPositiveFeedbackPage1] = useSessionStorage("positiveFeedbackPage1", "")
-    const [negativeFeedbackPage1, setNegativeFeedbackPage1] = useSessionStorage("negativeFeedbackPage1", "")
-
-    const [qualityRatingPage2, setQualityRatingPage2] = useSessionStorage("qualityRatingPage2", null)
-    const [positiveFeedbackPage2, setPositiveFeedbackPage2] = useSessionStorage("positiveFeedbackPage2", "")
-    const [negativeFeedbackPage2, setNegativeFeedbackPage2] = useSessionStorage("negativeFeedbackPage2", "")
-
-    const [qualityRatingPage3, setQualityRatingPage3] = useSessionStorage("qualityRatingPage3", null)
-    const [positiveFeedbackPage3, setPositiveFeedbackPage3] = useSessionStorage("positiveFeedbackPage3", "")
-    const [negativeFeedbackPage3, setNegativeFeedbackPage3] = useSessionStorage("negativeFeedbackPage3", "")
-
-
+    const [qualityRatingPage3, setQualityRatingPage3] = useSessionStorage<number | undefined>("qualityRatingPage3", undefined)
+    const [positiveFeedbackPage3, setPositiveFeedbackPage3] = useSessionStorage<string>("positiveFeedbackPage3", "")
+    const [negativeFeedbackPage3, setNegativeFeedbackPage3] = useSessionStorage<string>("negativeFeedbackPage3", "")
 
 
     const SurveyStarterPage = () => {
         return (
             <SurveyPage
-                title="Шановні мешканці ЖК «Балківський»!"
-                subtitle="Ми, керуюча компанія Bona Vita, цінуємо вашу думку і прагнемо постійно покращувати якість послуг, які ми надаємо.
-
-Опитування надає цінний зворотний зв'язок, який допоможе нам зрозуміти ваше задоволення і вподобання, а також дізнатися про сфери, в яких ми можемо вдосконалюватися."
-                ratingLabel="1. Оцініть, будь ласка, рівень роботи інтернет-провайдерів та системи безпеки
-"
-                italicSubtitle="(висловіть свою думку про якість послуг інтернет-провайдерів, роботу камер спостереження, міток доступу та шлагбаумів, щоб забезпечити надійну систему безпеки для всіх мешканців)"
-                positiveLabel="Що Вам подобається найбільше?"
-                negativeLabel="Що Вам не подобається найбільше?"
-
-                onNextStep={ROUTES.SurveySecond}
+                title={SURVEY_TEXTS.starter.title}
+                subtitle={SURVEY_TEXTS.starter.subtitle}
+                ratingLabel={SURVEY_TEXTS.starter.ratingLabel}
+                italicSubtitle={SURVEY_TEXTS.starter.italicSubtitle}
                 qualityRating={qualityRatingPage1}
                 setQualityRating={setQualityRatingPage1}
                 positiveFeedback={positiveFeedbackPage1}
                 setPositiveFeedback={setPositiveFeedbackPage1}
                 negativeFeedback={negativeFeedbackPage1}
                 setNegativeFeedback={setNegativeFeedbackPage1}
+
+                onNextStep={ROUTES.SurveySecond}
+
+
             />
         )
     }
@@ -52,9 +45,8 @@ function App() {
     const SurveySecond = () => {
         return (
             <SurveyPage
-                ratingLabel="2. Оцініть, будь ласка, рівень роботи будівельної компанії «Стікон»"
-                italicSubtitle="(висловіть свою думку про якість будівельних робіт, дотримання строків та якість виконання проєктів «Стікон»)"
-                showReturnButton={true}
+                ratingLabel={SURVEY_TEXTS.second.ratingLabel}
+                italicSubtitle={SURVEY_TEXTS.second.italicSubtitle}
                 onNextStep={ROUTES.SurveyThird}
                 qualityRating={qualityRatingPage2}
                 setQualityRating={setQualityRatingPage2}
@@ -62,6 +54,8 @@ function App() {
                 setPositiveFeedback={setPositiveFeedbackPage2}
                 negativeFeedback={negativeFeedbackPage2}
                 setNegativeFeedback={setNegativeFeedbackPage2}
+                showReturnButton={true}
+
 
 
             />
@@ -73,18 +67,15 @@ function App() {
     const SurveyThird = () => {
         return (
             <SurveyPage
-
-                ratingLabel="3. Оцініть, будь ласка, рівень роботи управляючої компанії Bona Vita"
-                italicSubtitle="(оцінка якості обслуговування, оперативності реагування на заявки, рівня чистоти та благоустрою території, а також ефективності комунікації з нами)"
+                ratingLabel={SURVEY_TEXTS.third.ratingLabel}
+                italicSubtitle={SURVEY_TEXTS.third.italicSubtitle}
                 showReturnButton={true}
-                onNextStep={"/"}
-
-                qualityRating={qualityRatingPage1}
-                setQualityRating={setQualityRatingPage1}
-                positiveFeedback={positiveFeedbackPage1}
-                setPositiveFeedback={setPositiveFeedbackPage1}
-                negativeFeedback={negativeFeedbackPage1}
-                setNegativeFeedback={setNegativeFeedbackPage1}
+                qualityRating={qualityRatingPage3}
+                setQualityRating={setQualityRatingPage3}
+                positiveFeedback={positiveFeedbackPage3}
+                setPositiveFeedback={setPositiveFeedbackPage3}
+                negativeFeedback={negativeFeedbackPage3}
+                setNegativeFeedback={setNegativeFeedbackPage3}
 
             />
 
@@ -102,11 +93,11 @@ function App() {
 
                 <Route
                     path={ROUTES.SurveySecond}
-                    element={<SurveySecond showReturnButton={true} />} />
+                    element={<SurveySecond/>} />
 
                 <Route
                     path={ROUTES.SurveyThird}
-                    element={<SurveyThird showReturnButton={true} />} />
+                    element={<SurveyThird />} />
 
 
             </Routes>
--- frontend/src/pages/Survey/Survey.tsx
+++ frontend/src/pages/Survey/Survey.tsx
@@ -1,37 +1,51 @@
-import React, { useState } from "react"
+import React, { ChangeEvent, useState } from "react"
 import RatingScale from "../../components/RatingScale/RatingScale"
 import InputArea from "../../components/InputArea/InputArea"
 import { MainButton } from "@vkruglikov/react-telegram-web-app"
 import { useNavigate } from "react-router-dom"
+import ReturnButton from "../../components/ReturnButton/ReturnButton.tsx"
 import "./Survey.scss"
-import ReturnButton from "../../components/ReturnButton/ReturnButton.tsx" // Переконайтеся, що шлях до компоненту вірний
+import {COMMON_LABELS} from "../../constants/texts.ts"
 
-function SurveyPage({
+
+const SurveyPage: React.FC<SurveyPageProps> = ({
     title,
     subtitle,
     ratingLabel,
     onNextStep,
+    italicSubtitle,
     qualityRating,
     setQualityRating,
     positiveFeedback,
     setPositiveFeedback,
     negativeFeedback,
     setNegativeFeedback,
-    italicSubtitle,
     showReturnButton,
-}) {
-    const positiveLabel = "Що Вам подобається найбільше?"
-    const negativeLabel = "Що Вам не подобається найбільше?"
+}) => {
     const navigate = useNavigate()
-    const [currentRating, setCurrentRating] = useState(qualityRating)
+
+    const [localCurrentRating, setLocalCurrentRating] = useState<number | undefined>(qualityRating)
+    const [localPositiveFeedback, setLocalPositiveFeedback] = useState<string>(positiveFeedback)
+    const [localNegativeFeedback, setLocalNegativeFeedback] = useState<string>(negativeFeedback)
+
+    const handlePositiveFeedbackChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setLocalPositiveFeedback(event.target.value)
+    }
+
+    const handleNegativeFeedbackChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setLocalNegativeFeedback(event.target.value)
+    }
+
+    const handleRatingChange = (newValue: number | number[]) => {
+        if (typeof newValue === "number") {
+            setLocalCurrentRating(newValue)
+        }
+    }
 
     const handleNext = () => {
-        setQualityRating(currentRating)
-        setPositiveFeedback(positiveFeedback)
-        setNegativeFeedback(negativeFeedback)
-
-        console.log(onNextStep)
-
+        if (localCurrentRating !== undefined) setQualityRating(localCurrentRating)
+        setPositiveFeedback(localPositiveFeedback)
+        setNegativeFeedback(localNegativeFeedback)
         navigate(onNextStep)
     }
 
@@ -39,28 +53,28 @@ function SurveyPage({
         <div className="survey-page">
             {title && <h1 className="survey-page__title">{title}</h1>}
             {subtitle && <p className="survey-page__text">{subtitle}</p>}
-            {ratingLabel && (
-                <div className="survey-page__rating-label">
-                    {ratingLabel}
-                </div>
-            )}
+            {ratingLabel && <div className="survey-page__rating-label">{ratingLabel}</div>}
             {italicSubtitle && <p className="survey-page__italic-text">{italicSubtitle}</p>}
 
             <RatingScale
                 name="quality"
-                onRatingChange={setCurrentRating}
+                value={localCurrentRating}
+                onRatingChange={handleRatingChange}
             />
+
             <InputArea
-                label={positiveLabel}
-                value={positiveFeedback}
-                onChange={(e) => setPositiveFeedback(e.target.value)}
+                label={COMMON_LABELS.positiveLabel}
+                value={localPositiveFeedback}
+                onChange={handlePositiveFeedbackChange}
             />
+
             <InputArea
-                label={negativeLabel}
-                value={negativeFeedback}
-                onChange={(e) => setNegativeFeedback(e.target.value)}
+                label={COMMON_LABELS.negativeLabel}
+                value={localNegativeFeedback}
+                onChange={handleNegativeFeedbackChange}
             />
-            {currentRating !== null && (
+
+            {localCurrentRating !== undefined && (
                 <MainButton
                     text="Далі"
                     onClick={handleNext}
